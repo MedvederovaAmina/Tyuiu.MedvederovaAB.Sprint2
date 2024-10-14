3ac5d9404e162999357cb395534fd8f7292ebebb
Амина Медведерова <medvederovaamina@outlook.com>
2024-10-15 00:22:27 +0500

    Task5

---
+++ Tyuiu.MedvederovaAB.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using Tyuiu.MedvederovaAB.Sprint2.Task5.V5.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #2  |  Выполнила: Медведерова А. Б.  |  ПКТб-24-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Тернарный оператор.                                               *");
+Console.WriteLine("* Задание: #5                                                             *");
+Console.WriteLine("* Вариант: #5                                                             *");
+Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКТб-24-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение и возвращает   *");
+Console.WriteLine("* результат.                                                              *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(" Введите номер карты: ");
+int numCard = Convert.ToInt32(Console.ReadLine());
+string res;
+if ((numCard < 6) || (numCard > 14))
+{
+    res = " Введено неверное значение! ";
+}
+else
+{
+    res = " Эта карта: " + ds.FindCardValue(numCard);
+}
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(res);
+Console.ReadKey();
