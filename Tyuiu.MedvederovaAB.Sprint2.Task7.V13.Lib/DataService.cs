985e40eb9adc2f03517d25e7cf052ee36ac9136b
Амина Медведерова <medvederovaamina@outlook.com>
2024-10-15 20:37:34 +0500

    Task7

---
+++ Tyuiu.MedvederovaAB.Sprint2.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MedvederovaAB.Sprint2.Task7.V13.Lib
+{
+    public class DataService : ISprint2Task7V13
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y <= 1) && (x - y <= 1) && (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1))
+            {
+                res = true;
+            }
+            else
+            {
+                 res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MedvederovaAB.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MedvederovaAB.Sprint2.Task7.V13.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint2.Task7.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2  |  Выполнила: Медведерова А. Б.  |  ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту.                *");
+            Console.WriteLine("* Задание: #7                                                             *");
+            Console.WriteLine("* Вариант: #13                                                            *");
+            Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами x,y в заштрихованной области *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Введите значение переменной x : ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" Введите значение переменной y : ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится вне заштрихованной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
